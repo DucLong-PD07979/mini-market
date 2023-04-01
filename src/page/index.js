52f207b58aeab8f90ca6c6696150439860bd9c94
Duc Long <longndpd07979@fpt.edu.vn>
2023-04-01 13:03:07 +0700

    tạo các trang thành phần và layout

---
+++ src/page/index.js
@@ -0,0 +1,4 @@
+export {default as Home} from './home/Home.jsx';
+export {default as ProducDetails} from './productDetails/ProductDetails.jsx';
+export {default as Search} from './search/Search.jsx';
+export {default as Profile} from './profile/Profile.jsx';
